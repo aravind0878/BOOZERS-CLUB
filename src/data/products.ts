8b337ee34bcb4be011f3011e4db3ac41049e4071
Aravind <ammulaaravind123@gmail.com>
2025-04-26 02:46:00 +0530

    m

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,10 +7,7 @@ export const products: Product[] = [
     name: "Arise Mélange Gray T-Shirt",
     price: 349,
     image: "/images/arise1.jpg",
-    description: "Experience comfort and style with our Mélange Gray T-Shirt. Made from a premium cotton-blend fabric, this tee offers a relaxed fit, subtle texture, and versatile look perfect for everyday wear. Dress it up or keep it casual — it’s a must-have essential in any wardrobe.
-
-",
-   
+    description: "Experience comfort and style with our Mélange Gray T-Shirt. Made from a premium cotton-blend fabric, this tee offers a relaxed fit, subtle texture, and versatile look perfect for everyday wear. Dress it up or keep it casual — it’s a must-have essential in any wardrobe.",
     sizes: ["s", "m", "l", "xl" ],
     additionalImages: ["/images/arise2.jpg", "/images/arise3.jpg"]
   },
@@ -20,7 +17,6 @@ export const products: Product[] = [
     price: 349,
     image: "/images/opr1.jpg",
     description: "Show your love for the globally popular anime and manga series One Piece with this vibrant red t-shirt, a comfortable and stylish way to express your fandom. Featuring the iconic Jolly Roger, the skull and crossbones symbol of the Straw Hat Pirates, this shirt is a must-have for any fan.This t-shirt is typically crafted from a soft and breathable fabric.",
-   
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/opr2.jpg"]
   },
@@ -30,17 +26,15 @@ export const products: Product[] = [
     price: 349,
     image: "/images/bzw1.jpg",
     description: "Show off a cool and casual vibe with this white t-shirt featuring a vibrant graphic that reads "Boozers Club" This tee is perfect for everyday wear and adding a touch of urban style to your look.",
-    
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/bzw2.jpg"]
   },
   {
     id: "arisew1",
-    name: "Arise White T-shirt,
+    name: "Arise White T-shirt",
     price: 349,
     image: "/images/arisew1.jpg",
     description: "Experience comfort and style with our Mélange Gray T-Shirt. Made from a premium cotton-blend fabric, this tee offers a relaxed fit, subtle texture, and versatile look perfect for everyday wear. Dress it up or keep it casual — it’s a must-have essential in any wardrobe.",
-    
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/arisew2.jpg"]
   },
@@ -50,7 +44,6 @@ export const products: Product[] = [
     price: 349,
     image:"/images/bzb1.jpg",
     description: "Show off a cool and casual vibe with this white t-shirt featuring a vibrant graphic that reads "Boozers Club" This tee is perfect for everyday wear and adding a touch of urban style to your look.",
-   
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/bzb2.jpg"]
   },
@@ -60,7 +53,6 @@ export const products: Product[] = [
     price: 349,
     image:"/images/opw1.jpg",
     description: "Show your love for the globally popular anime and manga series One Piece with this vibrant red t-shirt, a comfortable and stylish way to express your fandom. Featuring the iconic Jolly Roger, the skull and crossbones symbol of the Straw Hat Pirates, this shirt is a must-have for any fan.This t-shirt is typically crafted from a soft and breathable fabric.",
-   
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/opw2.jpg"]
   },
@@ -70,7 +62,6 @@ export const products: Product[] = [
     price: 349,
     image: "/images/bs1.jpg",
     description: "Show your appreciation for the iconic Bombay Sapphire gin with this stylish gray t-shirt. Featuring the distinctive Bombay Sapphire logo, this tee offers a subtle yet sophisticated way to express your taste. It's a comfortable and casual piece perfect for everyday wear.",
-   
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/bs2.jpg"]
   },
@@ -80,7 +71,6 @@ export const products: Product[] = [
     price: 349,
     image: "/images/bzblue1.jpg",
     description: "Show off a cool and casual vibe with this white t-shirt featuring a vibrant graphic that reads "Boozers Club" This tee is perfect for everyday wear and adding a touch of urban style to your look.",
-   
     sizes: ["s", "m", "l", "xl"],
     additionalImages: ["/images/bzblue2.jpg"]
   }
